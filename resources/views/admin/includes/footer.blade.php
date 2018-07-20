3b615e09cab67e89ffaf5151e11181b916109889
at-vyle <vy.le@asiantech.vn>
2018-07-20 16:18:12 +0700

    create template admin layout

---
+++ resources/views/admin/includes/footer.blade.php
@@ -0,0 +1,11 @@
+<!-- Jquery Core Js -->
+<script
+	src="admin/plugins/jquery/jquery.min.js"></script>
+<script
+	src="admin/plugins/bootstrap/js/bootstrap.js"></script>
+<!-- Custom Js -->
+<script
+	src="admin/js/admin.js"></script>
+</body>
+
+</html>
---
+++ resources/views/admin/includes/header.blade.php
@@ -0,0 +1,14 @@
+	<!-- Overlay For Sidebars -->
+	<div class="overlay"></div>
+	<!-- #END# Overlay For Sidebars -->
+	<!-- Top Bar -->
+	<nav class="navbar">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand"	href="">Welcome to Foody</a>
+			</div>
+			
+		</div>
+	</nav>
+  <!-- #Top Bar -->
+  
