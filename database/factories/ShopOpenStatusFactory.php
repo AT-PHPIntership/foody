95e0acc2d9430c59fc8f17b344653c16952adcd6
at-hienpham <hien.pham@asiantech.vn>
2018-07-23 16:26:28 +0700

    create migration and model

---
+++ database/factories/ShopOpenStatusFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\ShopOpenStatus::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2018_07_23_072845_create_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')
+                    ->references('id')->on('users')
+                    ->onDelete('no action');
+            $table->string('address')->charset('utf8')->collation('utf8_unicode_ci');
+            $table->unsignedInteger('money_ship');
+            $table->date('submit_time');
+            $table->date('delivery_time');
+            $table->string('customer_note')->charset('utf8')->collation('utf8_unicode_ci');
+            $table->unsignedInteger('status');
+            $table->timestamps();
+            $table->softDeletes('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
