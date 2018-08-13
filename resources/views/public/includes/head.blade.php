c6b11513a43ebb8f207216c071b489e1af10545b
at-vyle <vy.le@asiantech.vn>
2018-08-13 15:39:22 +0700

    layout index user

---
+++ resources/views/public/includes/head.blade.php
@@ -0,0 +1,71 @@
+  <meta name="robots" content="index, follow, all" />
+  <meta name="p:domain_verify" content="73eb0f0e066e6b151806fd693a1fbf35" />
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <link rel="icon" href="favicon.ico" type="image/x-icon">
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width" />
+  <title>FlyFood quảng b&#225; ẩm thực m&#243;n ăn truyền thống Việt Nam</title>
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" media="all" type="text/css" />
+  <link href="Content/bootstrap/dist/css/bootstrap.css" rel="stylesheet"/>
+  <link href="Content/font-awesome/css/font-awesome.css" rel="stylesheet"/>
+
+  <link href="Content/site.css" rel="stylesheet"/>
+  <script src="Scripts/jquery-1.11.1.js"></script>
+  <script src="Scripts/modernizr-2.6.2.js"></script>
+
+  <link href="Content/jquery.mCustomScrollbar.css" rel="stylesheet" />
+  <link href="Content/owl.carousel.css" rel="stylesheet" />
+  <link href="Content/Responsive.css" rel="stylesheet" />
+  
+  <meta name="description" content="Quảng bá ẩm thực món ăn truyền thống Việt Nam, dịch vụ món ngon tận nhà tại TP Hồ Chí Minh" />
+  <meta name="keywords" content="FlyFood, ẩm thực truyền thống, món ngon tận nhà, món ăn truyền thống" />
+
+  <meta property="og:type" content="article" />
+  <meta property="og:url" content="http://flyfood.vn/" />
+  <meta property="og:image" content="http://flyfood.vn/Files/Images/flyfood-avarta.png" />
+  <meta property="og:title" content="Flyfood Quảng bá ẩm thực món ăn truyền thống Việt Nam" />
+  <meta property="og:description" content="Quảng bá  ẩm thực món ăn truyền thống Việt Nam, dịch vụ món ngon tận nhà tại TP Hồ Chí Minh" />
+
+
+  
+
+  <!--Start of Zendesk Chat Script-->
+  <script type="text/javascript">
+    window.$zopim || (function (d, s) {
+      var z = $zopim = function (c) { z._.push(c) }, $ = z.s =
+      d.createElement(s), e = d.getElementsByTagName(s)[0]; z.set = function (o) {
+        z.set.
+        _.push(o)
+      }; z._ = []; z.set._ = []; $.async = !0; $.setAttribute("charset", "utf-8");
+      $.src = "https://v2.zopim.com/?59bxSWoiJHvPJxV09F9EDXKSynWwx46A"; z.t = +new Date; $.
+      type = "text/javascript"; e.parentNode.insertBefore($, e)
+    })(document, "script");
+  </script>
+  <!--End of Zendesk Chat Script-->
+  
+  <!-- Chặn nội dung chữ trong bài viết  
+  <script type="text/JavaScript">
+      function killCopy(e)
+      {
+          return false;
+      }
+      function reEnable()
+      {
+          return true;
+      }
+      document.onselectstart = new Function("return false")
+      if (window.sidebar)
+      {
+          document.onmousedown = killCopy;
+          document.onclick = reEnable;
+      }
+  </script>
+  -->
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TQ2T2H9');</script>
+<!-- End Google Tag Manager -->
