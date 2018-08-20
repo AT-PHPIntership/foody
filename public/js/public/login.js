0b8782ec099c44d6359e880cba6d5c20b547ec3a
at-hienpham <hien.pham@asiantech.vn>
2018-08-20 15:09:41 +0700

    fix comment:login.js

--- public/js/public/login.js
+++ public/js/public/login.js
@@ -30,11 +30,9 @@ $(document).ready(function () {
         $('#userName').html(response.result.user.username);
         window.location.href = 'http://' + window.location.hostname;
       },
-      statusCode: {
-        401: function (response) {
-          alert(response.responseJSON.error);
-          $('.login-form input[type="password"]').val('');
-        }
+      error: function (response) {
+        alert(response.responseJSON.error);
+        $('.login-form input[type="password"]').val('');
       }
       });
   })
