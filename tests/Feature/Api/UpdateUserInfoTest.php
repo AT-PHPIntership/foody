087211c427419178d536631c2f1f12f140d1cab9
at-hienpham <hien.pham@asiantech.vn>
2018-08-28 10:54:58 +0700

    refactor

--- tests/Feature/Api/UpdateUserInfoTest.php
+++ tests/Feature/Api/UpdateUserInfoTest.php
@@ -22,8 +22,6 @@ class UpdateUserInfoTest extends TestCase
    public function setUp()
    {
        parent::setUp();
-       factory(User::class)->create();
-       Artisan::call('passport:install');
    }
 
    /**
@@ -63,10 +61,11 @@ class UpdateUserInfoTest extends TestCase
        ];
        $response = $this->json('POST', '/api/login', $login, ['Accept' => 'application/json']);
        $token = json_decode($response->getContent())->result->token;
-       $this->json('GET', 'api/users/info', [], ['Accept' => 'application/json', 'Authorization' => 'Bearer '.$token])
+       $this->jsonUser('GET', 'api/users/info',[], [])
            ->assertStatus(200)
            ->assertJsonStructure($this->jsonStructureGetProfile());
    }
+
     /**
     * Test check some object compare database.
     *
@@ -81,7 +80,7 @@ class UpdateUserInfoTest extends TestCase
        ];
        $responseLogin = $this->json('POST', '/api/login', $login, ['Accept' => 'application/json']);
        $token = json_decode($responseLogin->getContent())->result->token;
-       $responseProfie = $this->json('GET', 'api/users/info', [], ['Accept' => 'application/json', 'Authorization' => 'Bearer '.$token]);
+       $responseProfie = $this->jsonUser('GET', 'api/users/info',[], []);
 
        $data = json_decode($responseProfie->getContent())->result;
        $arrayUser = [
